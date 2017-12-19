2e81966b9578ff4c58f373002ad2f3cf5db20da3
Joe Rainer <kop0419@gmail.com>
2017-12-18 20:19:24 -0500

    Uploading blogger project in it's own repository

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,8 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id
 	end
 
 	def new
@@ -47,4 +49,7 @@ class ArticlesController < ApplicationController
   		redirect_to article_path(@article)
 
 	end
+
+
+
 end
